fb0e6d358a421f7878403a84e336bd458fe47b0d
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-07-11 12:14:29 +0200

    stack

--- Stack-Dojo/Program.cs
+++ Stack-Dojo/Program.cs
@@ -8,8 +8,26 @@ namespace Stack_Dojo
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //Just some testing, as i cant write tests :O
+            Stack<int> mystack = new Stack<int>(5);
+            mystack.Push(3);
+            mystack.Push(4);
+            mystack.Push(1);
+            Console.WriteLine(mystack.GetSize());
+            Console.WriteLine(mystack.FreeSpaceLeft());
+            Console.WriteLine("--");
+            Console.WriteLine(mystack.Pop());
+            Console.WriteLine(mystack.Pop());
+            Console.WriteLine(mystack.Pop());
+            Console.WriteLine("--");
+            mystack.Push(1);
+            Console.WriteLine(mystack.Pop());
+            Console.WriteLine("--");
+            Console.WriteLine(mystack.FreeSpaceLeft());
+            Console.ReadLine();
         }
     }
 }
---
+++ Stack-Dojo/Stack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Dojo
+{
+    class Stack<T>
+    {
+        private int Size { get; set; }
+        private int Position { get; set; }
+        private T[] stack;
+
+        public Stack(int Size)
+        {
+            this.Size = Size;
+            stack = new T[Size];
+            Position = 0;
+        }
+
+        public void Push(T value)
+        {
+            try
+            {
+                stack[Position] = value;
+                Position++;
+            }
+            catch
+            {
+                throw new ArgumentException("Stack is full!");
+            }
+        }
+
+        public T Pop()
+        {
+            if (Position > 0)
+            {
+                Position--;
+                T value = stack[Position];
+                return value;
+            }
+            else
+            {
+                throw new ArgumentException("No data in the stack");
+            }
+        }
+
+        public T Peek()
+        {
+            if (Position > 0)
+            {
+                Position--;
+                T value = stack[Position];
+                Position++;
+                return value;
+            }
+            else
+            {
+               throw new ArgumentException("No data in the stack");
+            }
+        }
+
+        public int GetSize()
+        {
+            return Size;
+        }
+
+        public int FreeSpaceLeft()
+        {
+            return Size - Position;
+        }
+
+    }
+}
